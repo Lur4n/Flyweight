c24e84a7cb5f7e4df7e2312462800cc952a243d3
Luan <luancarlos1201@gmail.com>
2025-04-26 19:44:48 -0300

    Update clsContexto.py

--- impFly/controle/clsContexto.py
+++ impFly/controle/clsContexto.py
@@ -27,7 +27,7 @@ class clsContexto:
     def gerar_sprites(self):
         """Gerar sprites enquanto o loop estiver ativo"""
         if self.loop_ativo:
-            intrinsic_state = "../img/cinirin.png"  # Caminho da imagem shared state(estado compartilhado)
+            intrinsic_state = "../img/peso.png"  # Caminho da imagem shared state(estado compartilhado)
             extrinsic_x, extrinsic_y =  random.randint(0, 1200), random.randint(0, 600) #posições aleatórias para o sprite
             #O PULO DO GATO TA AQUI
             sprite = self.FabricaSprite.get_Sprites(intrinsic_state)  #obtém ou cria o sprite da fábrica
@@ -58,14 +58,11 @@ class clsContexto:
             self.botao.config(text="Iniciar")
     
     def mais_pesos(self):
-        intrinsic_state2 = "../img/peso.png"
         intrinsic_state3 = "../img/ubuntu.png"  # Caminho da imagem shared state(estado compartilhasdi)
         intrinsic_state4 = "../img/lactea.png"  # Caminho da imagem shared state(estado compartilhasdi)
         
-        sprite2 = self.FabricaSprite.get_Sprites(intrinsic_state2)
         sprite3 = self.FabricaSprite.get_Sprites(intrinsic_state3)  # Obtém ou cria o sprite da fábrica
         sprite4 = self.FabricaSprite.get_Sprites(intrinsic_state4)
         
-        self.canvas.create_image(random.randint(0, 1200), random.randint(0, 600), image=sprite2.get_sprite())  # Cria a imagem no canvas
         self.canvas.create_image(random.randint(0, 1200), random.randint(0, 600), image=sprite3.get_sprite())
         self.canvas.create_image(random.randint(0, 1200), random.randint(0, 600), image=sprite4.get_sprite())  # Cria a imagem no canvas
